8bcedf0cc0f618360bd06669566f4e157dfb9ba0
IDilettant <dedenev.dev@gmail.com>
2024-04-02 00:18:55 +0700

    chore: add reels generate

--- services/generator/generator.go
+++ services/generator/generator.go
@@ -5,9 +5,11 @@ import (
 	"github.com/releaseband/golang-developer-test/internal/rng"
 )
 
+const gameBoardSize = 3
+
 type Symbols struct {
 	rowsCount int
-	gameTapes []symbols.Symbols
+	gameTapes []symbols.Symbols // symbols.Reels
 }
 
 func NewSymbols(rowsCount int, gameTapes []symbols.Symbols) *Symbols {
@@ -15,11 +17,42 @@ func NewSymbols(rowsCount int, gameTapes []symbols.Symbols) *Symbols {
 }
 
 func (s *Symbols) Generate(rng rng.RNG) (symbols.Reels, error) {
-	// todo: implement me
-	return nil, nil
+	reels := make(symbols.Reels, len(s.gameTapes))
+	for i, tape := range s.gameTapes {
+		indices := make(map[int]bool, len(tape))
+		length := uint32(len(tape) - 1)
+		for range tape {
+			indx := int(rng.Random(0, length))
+			if ok := indices[indx]; !ok {
+				indices[indx] = true
+				reels[i] = append(reels[i], tape[indx])
+
+				continue
+			}
+
+			for shift := indx + 1; len(reels[i]) != len(tape); shift++{
+				shift = shift % len(tape) - 1
+				if ok := indices[shift]; !ok {
+					indices[shift] = true
+					reels[i] = append(reels[i], tape[shift])
+
+					break
+				}
+			}
+		}
+	}
+
+	s.gameTapes = reels
+
+	return reels, nil
 }
 
 func (s *Symbols) GetReelSymbols(reelIndex int, rowIndex int) symbols.Symbols {
-	// todo: implement me
-	return nil
+	reelSymbols := make(symbols.Symbols, 0, gameBoardSize)
+	for i := rowIndex; len(reelSymbols) != gameBoardSize; i++ {
+		i = i % len(s.gameTapes[reelIndex])
+		reelSymbols = append(reelSymbols, s.gameTapes[reelIndex][i])
+	}
+
+	return reelSymbols
 }
