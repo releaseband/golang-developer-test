b94684bab25defcb2c11ddac1364e5b9174776b1
IDilettant <dedenev.dev@gmail.com>
2024-04-02 00:21:03 +0700

    chore: add wins calculate

--- services/calculator/calculator.go
+++ services/calculator/calculator.go
@@ -21,6 +21,38 @@ func NewCalculator(lines lines.Lines, payTable *paytable.PayTable) *Calculator {
 }
 
 func (c *Calculator) Calculate(spinSymbols symbols.Reels) ([]win.Win, error) {
-	// todo: implement me
-	return nil, nil
+	prizes := make([]win.Win, 0)
+	for _, line := range c.lines {
+		indices := line.GetIndices()
+		winLine := make(symbols.Symbols, 0, len(indices))
+
+		var count int
+		prev, curr := -1, -1
+		for i, indx := range indices {
+			curr = spinSymbols[i][indx]
+			if curr != prev && curr != WILD && prev != -1 {
+				break
+			}
+
+			winLine = append(winLine, curr)
+			count++
+			prev = curr
+		}
+
+		payout, err := c.payTable.Get(prev, count - 1)
+		if err != nil {
+			return nil, err
+		}
+
+		if payout > 0 {
+			prize := win.NewWin(payout, winLine, prev)
+			prizes = append(prizes, prize)
+		}
+	}
+
+	if len(prizes) == 0 {
+		return nil, nil
+	}
+
+	return prizes, nil
 }
