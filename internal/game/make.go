02ab938ab7d0465a3fe288406092fe533e2e9690
IDilettant <dedenev.dev@gmail.com>
2024-04-02 00:24:04 +0700

    chore: add slot creating

--- internal/game/make.go
+++ internal/game/make.go
@@ -1,11 +1,34 @@
 package game
 
+import (
+	"github.com/releaseband/golang-developer-test/internal/configs/lines"
+	"github.com/releaseband/golang-developer-test/internal/configs/paytable"
+	"github.com/releaseband/golang-developer-test/internal/configs/symbols"
+	"github.com/releaseband/golang-developer-test/services/calculator"
+	"github.com/releaseband/golang-developer-test/services/generator"
+)
+
 const rowsCount = 3
 
 func New() (*Slot, error) {
-	// todo: implement me
-	// read lines configs
-	// read pay table configs
-	// create services
-	return nil, nil
+	winLines, err := lines.ReadLines()
+	if err != nil {
+		return nil, err
+	}
+
+	payTable, err := paytable.ReadPayTable()
+	if err != nil {
+		return nil, err
+	}
+
+	reels, err := symbols.ReadReels()
+	if err != nil {
+		return nil, err
+	}
+
+	gen := generator.NewSymbols(rowsCount, reels)
+	calc := calculator.NewCalculator(winLines, payTable)
+	slot := newSlot(gen, calc, RoundCost(rowsCount))
+
+	return slot, nil
 }
